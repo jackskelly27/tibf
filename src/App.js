03f7699284b2f086ad934a10b4500ff211552ec4
RealLukeMartin <lukemartinwebdev@gmail.com>
2022-08-23 19:35:42 -0500

    add logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Texas Is Back Folks!</h1>
+      <img src="/logo.png" alt="longhorn logo" class="logo" />
       <AudioButton />
     </div>
   );
