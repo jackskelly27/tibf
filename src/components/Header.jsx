506d0a564bd558c98f83cb1cc39e0d2123c8affa
jackskelly27 <jacks.kelly@gmail.com>
2022-12-29 16:45:15 -0600

    Added NavLink to Header.jsx so HearIt and SeetIt are underlined if on those page(s), changed css to underline if so

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
 
 function Header() {
   return (
     <div class="header">
         <a href="/hearit" trel="noopener norefferer">
-          <h1>Hear It</h1>
+          <h1>
+            <NavLink to="/hearit" className={({ isActive }) => (isActive ? 'active' : undefined)}>
+              Hear It
+           </NavLink>
+          </h1>
         </a>
         <a href="/" trel="noopener noreferrer">
           <img src="/logo.png" alt="longhorn logo" class="logo"/>
         </a>
         <a href="/seeit" trel="noopener norefferer">
-          <h1>See It</h1>
+          <h1>
+            <NavLink to="/seeit" className={({ isActive }) => (isActive ? 'active' : undefined)}>
+              Seet It
+            </NavLink>
+          </h1>
         </a>
     </div>
   )
