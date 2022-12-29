36e4737bdd73f5be573b51a415d456bdbd0446d3
jackskelly27 <jacks.kelly@gmail.com>
2022-12-29 17:20:12 -0600

    Removed unncessary a tag and h1 for seeit and hearit in header, added another audio button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,15 @@ import {NavLink} from 'react-router-dom';
 function Header() {
   return (
     <div class="header">
-        <a href="/hearit" trel="noopener norefferer">
-          <h1>
-            <NavLink to="/hearit" className={({ isActive }) => (isActive ? 'active' : undefined)}>
-              Hear It
-           </NavLink>
-          </h1>
-        </a>
-        <a href="/" trel="noopener noreferrer">
-          <img src="/logo.png" alt="longhorn logo" class="logo"/>
-        </a>
-        <a href="/seeit" trel="noopener norefferer">
-          <h1>
-            <NavLink to="/seeit" className={({ isActive }) => (isActive ? 'active' : undefined)}>
-              See It
-            </NavLink>
-          </h1>
-        </a>
+      <NavLink to="/hearit" className={({ isActive }) => (isActive ? 'active navlink' : 'navlink')}>
+        Hear It
+      </NavLink>
+      <a href="/" trel="noopener noreferrer">
+        <img src="/logo.png" alt="longhorn logo" class="logo"/>
+      </a>
+      <NavLink to="/seeit" className={({ isActive }) => (isActive ? 'active navlink' : 'navlink')}>
+        See It
+      </NavLink>
     </div>
   )
 }
--- src/pages/HearItPage.jsx
+++ src/pages/HearItPage.jsx
@@ -6,7 +6,10 @@ function HearItPage() {
     return (
         <>
             <h1 className="title">Hear It</h1>
+            <h1>It All Started Week 1 of 2016 vs Notre Dame</h1>
             <AudioButton name={'tibf_original_Tessitore.mp3'} />
+            <h1>Sam's Proclamation at the 2018 Sugar Bowl</h1>
+            <AudioButton name={'Ehlinger_Were_Back_Sugar_Bowl_2018.mp3'} />
             <h1>Any Doubts?</h1>
             <AudioButton name={'Brandon_Walker_tsb.mp3'} />
             <h1>Remember Week 2 of 2005?</h1>
